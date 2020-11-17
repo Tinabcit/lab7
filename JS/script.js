8c1b9c5bf7c30ff93ca3cd6eade25f6c5b7a09df
Lab6 <tshabestari@my.bcit.ca>
2020-11-16 18:04:45 -0800

    Change the array of courses list

--- JS/script.js
+++ JS/script.js
@@ -5,5 +5,11 @@
 
 courses: {
     code: 'ACIT 1620',
-    
+    name: 'Web Fundemental Technologies',
+
+    code: 'ACIT 1420',
+    name: 'Introduction to System Administration',
+
+    code: 'Math 1310',
+    name: 'Math for IT'
 }
