e0b9f9987c4206afac211c0ccf48caea6d57d0cc
lab 6 <tshabestari@my.bcit.ca>
2020-11-24 12:53:44 -0800

    changes to javascript

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,2 @@
-
-
-
-
-
-courses: {
-    code: 'ACIT 1620',
-    name: 'Web Fundemental Technologies',
-
-    code: 'ACIT 1420',
-    name: 'Introduction to System Administration',
-
-    code: 'Math 1310',
-    name: 'Math for IT'
-}
+let coursesList = ["ACIT 1620", "ACIT 1420", "Math 1310"]
+console.log(coursesList[1]);
